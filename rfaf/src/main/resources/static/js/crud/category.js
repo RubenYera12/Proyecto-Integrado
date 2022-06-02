60935e05fb0e2b12328907106b7f0d4bd4db5782
Rubén María Yera Martín <rubenmaria.yera@bosonit.com>
2022-06-02 16:18:54 +0200

    Thymeleaf y Crud Categoria

--- rfaf/src/main/resources/static/js/crud/category.js
+++ rfaf/src/main/resources/static/js/crud/category.js
@@ -4,12 +4,18 @@ var app = {
     init: function() {
         app.initDatatable('#categories');
 
-        $("#save").click(function(){
-            app.save({
-                id: $('#id').val(),
-                nombre : $('#nombre').val()
+        $("#create").click(function(){
+            app.create({
+                name : $('#nombre').val()
             });
         });
+        $("#update").click(function(){
+            app.update({
+                id: $('#id').val(),
+                name : $('#nombre').val()
+            });
+        });
+
     },
     initDatatable : function(id) {
         app.table = $(id).DataTable({
@@ -26,13 +32,34 @@ var app = {
             ],
             buttons: [
                 {
+                    text : 'Crear',
+                    action : function(e, dt, node, config) {
+                        app.cleanForm();
+                        $('#update').hide();
+                        $('#create').show();
+                        $('#personaModal').modal();
+                    }
+                },
+                {
                     text : 'Editar',
                     action : function(e, dt, node, config) {
+                        $('#create').hide();
+                        $('#update').show();
                         var data = dt.rows('.table-active').data()[0];
                         app.setDataToModal(data);
                         $('#personaModal').modal();
                     }
-                }
+                },
+                {
+                    text : 'Eliminar',
+                    action : function(e, dt, node, config) {
+                        var data = dt.rows('.table-active').data()[0];
+                        if(confirm('¿Seguro que quieres eliminar la categoría '+data.nombre+'?')){
+                            app.delete(data.id)
+                        }
+                    }
+                },
+
             ]
         });
 
@@ -47,20 +74,68 @@ var app = {
     },
     setDataToModal : function(data) {
         $('#id').val(data.id);
-        $('#nombre').val(data.nombre);
+        $('#nombre').val(data.name);
     },
-    save : function(data) {
+    cleanForm : function() {
+             $('#id').val('');
+             $('#nombre').val('');
+    },
+    create : function(data) {
         $.ajax({
-            url: app.backend + '/save',
+            url: app.backend + '/create',
             data : JSON.stringify(data),
             method: 'POST',
             dataType : 'json',
             contentType: "application/json; charset=utf-8",
             success : function(json) {
-                $("#msg").text('Se guardó la persona correctamente');
+                $("#msg").text('Se guardó la categoría correctamente');
                 $("#msg").show();
                 $('#personaModal').modal('hide');
                 app.table.ajax.reload();
+                setTimeout(function(){
+                    $("#msg").hide();
+                }, 5000);
+            },
+            error : function(error) {
+
+            }
+        })
+    },
+    update : function(data) {
+          $.ajax({
+              url: app.backend + '/update/'+data.id,
+              data : JSON.stringify(data),
+              method: 'PUT',
+              dataType : 'json',
+              contentType: "application/json; charset=utf-8",
+              success : function(json) {
+                  $("#msg").text('Se actualizó la categoría correctamente');
+                  $("#msg").show();
+                  $('#personaModal').modal('hide');
+                  app.table.ajax.reload();
+                  setTimeout(function(){
+                      $("#msg").hide();
+                  }, 5000);
+              },
+              error : function(error) {
+
+              }
+          })
+      },
+    delete : function(id) {
+        $.ajax({
+            url: app.backend + '/delete/'+id,
+            data : JSON.stringify(data),
+            method: 'DELETE',
+            dataType : 'json',
+            contentType: "application/json; charset=utf-8",
+            success : function(json) {
+                $("#msg").text('Se eliminó la categoría correctamente');
+                $("#msg").show();
+                app.table.ajax.reload();
+                setTimeout(function(){
+                    $("#msg").hide();
+                }, 5000);
             },
             error : function(error) {
 
